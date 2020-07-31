53944bb622f4012e3610b2fde477433340dcff15
armin <armin.schweitzer@phys.ethz.ch>
2020-07-31 18:39:59 +0200

    added routine for eval of formafactors. cleaned-up

--- ComputationFormFacGGHG/fortran_bridge_ggHg_QCD.cpp
+++ ComputationFormFacGGHG/fortran_bridge_ggHg_QCD.cpp
@@ -13,7 +13,7 @@ std::string exec(const char* cmd) {
     char buffer[128];
     std::string result = "";
     FILE* pipe = popen(cmd, "r");
-    if (!pipe) throw std::runtime_error("popen() failed!");
+    if (!pipe) throw std::runtime_error("popen() in fortran_bridge_ggHg_QCD.cpp failed!");
     try {
         while (!feof(pipe)) {
             if (fgets(buffer, 128, pipe) != NULL)
@@ -31,23 +31,22 @@ inline bool exists (const std::string& name) {
     return ( access( name.c_str(), F_OK ) != -1 );
 }
 
-extern "C" {
-//Fortran interface
-  void get_gggh_tensor_coefs(const double *   pInput,
-		                const double &   massTop,
-						const double &   massHiggs,
-						double *   oneLoopTensorRe,
-						double *   oneLoopTensorIm
-						)
-  {		
+
+
+extern"C" void get_gggh_tensor_coefs_fortran_(const double *pInput,
+		                const double &	massTop,
+						const double &	massHiggs,
+						double *oneLoopTensorRe,
+						double *oneLoopTensorIm
+						)  {
 		int npart = 4;
 	    double p[4][4];
 		for (int i=0; i<npart*4; i=i+4) {
 			for (int j=0; j<4; j++) {
-				p[(i/4)][j]=pInput[i+j];		  
+				p[(i/4)][j]=pInput[i+j];
 			}
 		}
-	  
+	  // we have to enforce that p[2] describes the gluon!
 	  double s, t;
 	  s =  (p[0][0]+p[1][0])*(p[0][0]+p[1][0]) - (p[0][1]+p[1][1])*(p[0][1]+p[1][1]) - (p[0][2]+p[1][2])*(p[0][2]+p[1][2]) - (p[0][3]+p[1][3])*(p[0][3]+p[1][3]);
       t =  (p[0][0]-p[2][0])*(p[0][0]-p[2][0]) - (p[0][1]-p[2][1])*(p[0][1]-p[2][1]) - (p[0][2]-p[2][2])*(p[0][2]-p[2][2]) - (p[0][3]-p[2][3])*(p[0][3]-p[2][3]);
@@ -61,40 +60,38 @@ extern "C" {
 	if (exists("./mathematicaRoutines/evaluation_amp.wls")) {
 		ostr<<"./mathematicaRoutines/evaluation_amp.wls ";
 	} else  {
-	   std::cerr<<"Could Not find 'evaluation_amp.wls'. Place it somewhere as defined in fortran_bridge_gggh.cpp"<<std::endl;
+	   std::cerr<<"Could Not find 'evaluation_amp.wls'. Place as defined in fortran_bridge_ggHg_QCD.cpp"<<std::endl;
        exit (EXIT_FAILURE);
 	}
 	ostr<< s << " ";
     ostr<<t<<" ";
-    ostr<<massTop<<" ";
-    ostr<<massHiggs;
+    ostr<<massHiggs<<" ";
+    ostr<<massTop;
 
 	std::string command = ostr.str();
-	std::cout<<"About to call wrapper with: "<<command<<std::endl;	
+	// std::cout<<"About to call wrapper with: "<<command<<std::endl;
     std::string str_result = exec(command.c_str());
-	std::cout<<"I got result="<<str_result<<std::endl;
+	// std::cout<<"I got result="<<str_result<<std::endl;
     vector<std::string> result;
 	std::istringstream iss(str_result);
 	bool output_stream_on = false;
 	for(std::string s; iss >> s; ) {
-//		std::cout<<"s="<<s<<std::endl;
+	//		std::cout<<"s="<<s<<std::endl;
 		if (s.compare("END_OUTPUT_STREAM")==0)
 			output_stream_on = false;
 		if (output_stream_on)
-		  result.push_back(s);			
+		  result.push_back(s);
 		if (s.compare("START_OUTPUT_STREAM")==0)
 			output_stream_on = true;
 	}
 
-	for(int i=0; i<4; i++) {
-		oneLoopTensorRe[i]=std::stod(result[i]);
-// std::cout<<"oneLoopTensorRe["<<i<<"]="<<oneLoopTensorRe[i]<<std::endl;		
-	}
-	for(int i=4; i<8; i++) {
-		oneLoopTensorIm[i-4]=std::stod(result[i]);		
-// std::cout<<"oneLoopTensorIm["<<i-4<<"]="<<oneLoopTensorIm[i-4]<<std::endl;				
-	}
+	// for(int i=0; i<4; i++) {
+	// 	oneLoopTensorRe[i]=std::stod(result[i]);
+	// 	std::cout<<"oneLoopTensorRe["<<i<<"]="<<oneLoopTensorRe[i]<<std::endl;
+	// }
+	// for(int i=4; i<8; i++) {
+	// 	oneLoopTensorIm[i-4]=std::stod(result[i]);
+	// 	std::cout<<"oneLoopTensorIm["<<i-4<<"]="<<oneLoopTensorIm[i-4]<<std::endl;
+	// }
 
-  }
 }
-
---
+++ ComputationFormFacGGHG/set_coefficient_ggHg_QCD.f
@@ -0,0 +1,25 @@
+      subroutine set1LoopGGHGQCDCoefficients(P,pmass) 
+        include 'coupl.inc'
+
+            double precision P(16)
+            double precision oneLoopTensorRe(4) 
+            double precision oneLoopTensorIm(4)
+            double precision mH, mT
+            ! ask val why we save
+            save oneLoopTensorRe
+            save oneLoopTensorIm
+
+            call get_gggh_tensor_coefs_fortran(P,mT,mH,
+     &     oneLoopTensorRe,oneLoopTensorIm)
+!         Update couplings
+            GGGH_ForFac1_RE  = real(oneLoopTensorRe(1) ,16)
+            GGGH_ForFac2_RE  = real(oneLoopTensorRe(2) ,16)
+            GGGH_ForFac3_RE  = real(oneLoopTensorRe(3) ,16)
+            GGGH_ForFac4_RE  = real(oneLoopTensorRe(4) ,16)
+            GGGH_ForFac1_IM  = real(oneLoopTensorIm(1) ,16)
+            GGGH_ForFac2_IM  = real(oneLoopTensorIm(2) ,16)
+            GGGH_ForFac3_IM  = real(oneLoopTensorIm(3) ,16)
+            GGGH_ForFac4_IM  = real(oneLoopTensorIm(4) ,16)
+            call COUP()
+
+      end subroutine set1LoopGGHGQCDCoefficients
