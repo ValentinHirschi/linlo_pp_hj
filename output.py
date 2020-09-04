568a3d6681efb9e0b04dce8b4c950bbbdfca55d5
ValentinHirschi <valentin.hirschi@gmail.com>
2020-09-04 13:02:55 +0200

    Add -fPIC to the C++ compilation of the fortran bridge.

--- output.py
+++ output.py
@@ -497,7 +497,7 @@ class My_ggHg_Exporter(export_v4.ProcessExporterFortranSA):
         # fix model makefile
         fix_make = """
 GCC=g++
-GCCFLAGS = $(MACFLAG) -Wall -std=c++11
+GCCFLAGS = $(MACFLAG) -Wall -std=c++11 -fPIC
 fortran_bridge%.o : fortran_bridge%.cpp
 \t$(GCC) -c $< $(GCCFLAGS)"""
         with open(pjoin(self.dir_path, 'Source','MODEL','makefile'),'a') as ff:
