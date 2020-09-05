01e81bceddbff69cc7135717326fc41f99dc8b0f
ValentinHirschi <valentin.hirschi@gmail.com>
2020-09-05 10:32:07 +0200

    Updated command to kill all MM processes.

--- GridFiller/kill_mathematica_processes.sh
+++ GridFiller/kill_mathematica_processes.sh
@@ -1,2 +1,3 @@
-killall -9 wolframscript
-killall -9 /opt/mathematica-12.0/Executables/wolframscript
+ps -ef | grep 'mathem' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+# killall -9 wolframscript
+# killall -9 /opt/mathematica-12.0/Executables/wolframscript
