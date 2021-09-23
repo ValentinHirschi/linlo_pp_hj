bbb02a8168d97d55dd45813b6ad2022b92ef9f3e
Martijn Hidding <martijn.hidding@physics.uu.se>
2021-09-23 17:40:37 +0200

    Preliminary setup for the computation of the amplitude.

---
+++ ComputationFormFac_PPHJ_GGHG/mathematicaRoutines/InterpolationDataLinker.m
@@ -0,0 +1,26 @@
+(* ::Package:: *)
+
+(* ::Input::Initialization:: *)
+{
+   Association[ {(* s,t, pp4, mb^2, mt^2, nloop, nf, is_HEFT, inc_ytqcd, inc_ytmb, inc_ytmt, inc_ybqcd, inc_ybmb, inc_ybmt *)
+		"mh" -> 125,
+		"mb" -> 0,
+		"mt" -> 125*Sqrt[23/12],
+		"nloop" ->1|2,
+		"nf" ->5,
+		"is_HEFT" -> False,
+		"inc_ytqcd" ->True,
+		"inc_ytmb" ->False,
+		"inc_ytmt" ->True,
+		"inc_ybqcd" -> False,
+		"inc_ybmb" ->False,
+		"inc_ybmt" ->  False,
+(* Conditions on s and t (such that n, m > 0) *)
+	"stRange" -> s>15876&&t>1/200 (3109375-199 s)
+}]->{
+(* Replacement for n,m in the interpolation functions *)
+	{n->-42+Sqrt[s]/3,m->(-349643-1053108 n-12537 n^2-1400 t)/(-16566-49896 n-594 n^2)},
+(* Data for the interpolations *)
+	"./Data/AmplitudeInterpolation1.mx"
+    }
+}//Association
--- ComputationFormFac_PPHJ_GGHG/mathematicaRoutines/test_me.sh
+++ ComputationFormFac_PPHJ_GGHG/mathematicaRoutines/test_me.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
-./gghg_coefs.wls 1.0 2.0 3.0 4.0 5.0 6.0 7.0 2 4 1 1 0 1 0 1 1 res.dat
+                             #s    t             mh  mb mt             yb yt mu nloop nf is_HEFT inc_ytqcd inc_ytmb inc_ytmt inc_ybqcd inc_ybmb inc_ybmt
+wolframscript gghg_coefs.wls 24336 -9259793/1400 125 0 125*Sqrt[23/12] 1 1 2 2 5 0 1 0 1 0 0 0 res.dat
